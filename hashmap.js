206eac69efac97b30926f8381c58d793b8fce2f1
Brian Bud <hello@brianbud.com>
2024-01-25 21:32:56 -0500

    hash method using prime number

--- hashmap.js
+++ hashmap.js
@@ -1,3 +1,13 @@
 if (index < 0 || index >= buckets.length) {
   throw new Error('Trying to access index out of bound');
 }
+
+function Hashmap() {
+  function hash(string) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < string.length; i++) {
+      hashCode = primeNumber * hashCode + string.charCodeAt(i);
+    }
+  }
+}
